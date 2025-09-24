823ee3c949661a7164cf8b784fd9d65cc75f6418
Carol-stats <93337839+Carol-stats@users.noreply.github.com>
2025-09-24 10:14:47 -0700

    Real Data Driver Files
    Added new files for real data read-in and use.
    Compares against ODE initialization rather than ground truth.

---
+++ inference_realdata.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#SBATCH -N 1
+#SBATCH -c 4
+#SBATCH --mem=264G
+#SBATCH -t 0-96:00:00
+
+#SBATCH -p general                      
+#SBATCH -q public
+#SBATCH -G a100:1
+
+#SBATCH -o /scratch/cylu1/logs/adaptive_inference/slurm.%j.out
+#SBATCH -e /scratch/cylu1/logs/adaptive_inference/slurm.%j.err
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=cylu1@asu.edu
+#SBATCH --export=NONE
+
+# === Load Environment ===
+module purge
+module load cuda-12.6.1-gcc-12.1.0
+module load mamba/latest
+source activate repopgraphing
+
+# === Set Up Scratch Directory ===
+export SCRATCH_RUN_DIR=/scratch/cylu1/inference_corrected_realdata_$SLURM_JOB_ID
+mkdir -p "$SCRATCH_RUN_DIR"
+cp -r ~/Celegans_inference_real/* "$SCRATCH_RUN_DIR"/
+cd "$SCRATCH_RUN_DIR"
+
+# === Ensure local imports work ===
+export PYTHONPATH=$PWD:$PYTHONPATH
+
+# === Use Selected Dataset ===
+DATA_FILE="real_data/Exp_1_Ecoli_population.csv"
+
+# === Run Script ===
+python inference_realdata.py "$DATA_FILE"
