2c3eef069ccd77c16cbbb3ffd1286e7a4f08f7c6
Pessoa <ppessoa@asu.edu>
2026-03-12 12:10:28 -0700

    pip

--- mcmc_bash_Jay.sh
+++ mcmc_bash_Jay.sh
@@ -21,7 +21,7 @@ module load cuda-12.6.1-gcc-12.1.0
 module load mamba/latest
 
 pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
-pip install numpy pandas scipy matplotlib scikit-learn tqdm nbconvert
+pip install numpy pandas scipy matplotlib scikit-learn tqdm nbconvert tqdm
 
 
 # python run_mcmc.py real_data/Exp_1_live_lowpH.csv \
